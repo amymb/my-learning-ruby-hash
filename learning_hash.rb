31ae0300bc6a52f7a8544bf2fea950c056bff743
Amy Bertken <amy.bertken@gmail.com>
2015-02-10 10:42:14 -0700

    finished exercise

--- learning_hash.rb
+++ learning_hash.rb
@@ -1,16 +1,53 @@
 # Any method in http://ruby-doc.org/core-2.2.0/Hash.html or http://ruby-doc.org/core-2.2.0/String.html is fair game to use. It is encouraged that you peruse through both as you work through the problems.
 
 class LearningHash
-  def foo_key(hash)
-    # your code here
+  def foo_key(x)
+    x[:foo]
   end
 
   def bar_key(hash)
-    # your code here
+    hash[:bar]
   end
 
   def key_stringer(hash)
-    # your code here
-    # hint see http://ruby-doc.org/core-2.1.5/Hash.html#method-i-each_key
+    stringer = ""
+    hash.each_key do |key|
+      x = key.to_s
+      stringer = stringer + x
+    end
+   stringer
   end
+
+  def value_stringer(hash)
+   stringy = ""
+   hash.each_value do |v|
+     stringy << v.to_s
+   end
+   stringy
+  end
+
+  def key_and_value_stringer(hash)
+    stringer = ""
+    hash.each_pair do |k, v|
+      stringer << k.to_s + v.to_s
+    end
+    stringer
+  end
+
+  def reversed_key_and_value_stringer(hash)
+    stringer = ""
+    hash.each_pair do |k, v|
+      stringer = stringer + k.to_s + v.to_s
+    end
+    stringer.reverse
+  end
+
+  def polite_is_empty?(hash)
+      if hash.empty?
+        "Yes ma'am"
+      else
+        "No ma'am"
+    end
+  end
+
 end
